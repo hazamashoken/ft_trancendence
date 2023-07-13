948a79707c5e4f3ba3f504a37935c1ddeb258bd3
tliangso <earth78203@gmail.com>
2023-07-13 10:48:31 +0700

    ADD swagger docs for backend
    - ADD swagger docs
    - REMOVE compoc/doc

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ft_Transencdence')
+    .setDescription('Backend API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
