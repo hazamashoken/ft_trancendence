94950cfbe1a9d75d600f773d1011562d620f3576
Araiva <kizzaraiva@gmail.com>
2023-07-23 17:38:25 +0700

    test: init profile test

--- backend/app/test/profiles/profiles.e2e-spec.ts
+++ backend/app/test/profiles/profiles.e2e-spec.ts
@@ -1,40 +1,78 @@
 import * as request from 'supertest';
 import { app, appDataSource, server, testSetup } from '../test-setup';
+import { describe } from 'node:test';
+import { Profile } from '@backend/typeorm/profile.entity';
 
 testSetup();
 
 describe('ProfileController (e2e)', () => {
-  test('/profile (GET)', async () => {
-    // const data = await appDataSource.manager.find(Profile);
-    return request(server)
-      .get('/profiles')
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({
-              id: expect.any(Number),
-              email: expect.any(String),
-            }),
-          ]),
-        );
-      });
+  describe('GET /profiles', () => {
+    test('/profile (GET)', async () => {
+      return request(server)
+        .get('/profiles')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                id: expect.any(Number),
+                email: expect.any(String),
+              }),
+            ]),
+          );
+        });
+    });
   });
 
-  test('/profile (GET)', async () => {
-    // const data = await appDataSource.manager.find(Profile);
-    return request(app.getHttpServer())
-      .get('/profiles')
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toEqual(
-          expect.arrayContaining([
+  describe('GET /profiles/:id', () => {
+    test('GET /profile/:id Succeed', async () => {
+      return request(app.getHttpServer())
+        .get('/profiles/1')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).toEqual(
             expect.objectContaining({
-              id: expect.any(Number),
-              email: expect.any(String),
+              id: 1,
+              email: 'tsomsa@student.42bangkok.com',
+              login: 'tsomsa',
             }),
-          ]),
-        );
+          );
+        });
+    });
+    test('GET /profile/:id NotFound', async () => {
+      return request(app.getHttpServer()).get('/profiles/0').expect(404);
+    });
+    test('GET /profile/:id BadRequest', async () => {
+      return request(app.getHttpServer()).get('/profiles/0').expect(400);
+    });
+  });
+
+  describe('POST /profiles/:id', () => {
+    const profileRepository = appDataSource.manager.getRepository(Profile);
+    const inputBody = {
+      login: 'tester',
+      email: 'tester@student.42.bangkok.com',
+    };
+
+    afterEach(async () => {
+      const profile = await profileRepository.findOne({
+        where: { login: 'tester' },
       });
+      console.log(profile);
+      if (profile) {
+        await profileRepository.remove(profile);
+      }
+    });
+
+    test('POST /profiles/:id Succeed', async () => {
+      await request(app.getHttpServer())
+        .post('/profiles/')
+        .send(inputBody)
+        .expect(201);
+      const profile = await profileRepository.findOne({
+        where: { login: 'tester' },
+      });
+      expect(profile).toBe(expect.objectContaining(profile));
+    });
   });
 });
--- backend/app/test/test-setup.ts
+++ backend/app/test/test-setup.ts
@@ -9,18 +9,22 @@ let app: INestApplication;
 export function testSetup() {
   beforeAll(async () => {
     await appDataSource.initialize();
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-    app = moduleFixture.createNestApplication();
-    await app.init();
-    server = app.getHttpServer();
+    await initServer();
   });
 
   afterAll(async () => {
     await appDataSource.destroy();
     await app.close();
   });
+}
+
+export async function initServer() {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+  app = moduleFixture.createNestApplication();
+  await app.init();
+  server = app.getHttpServer();
 }
 
 export { app, server, appDataSource };
