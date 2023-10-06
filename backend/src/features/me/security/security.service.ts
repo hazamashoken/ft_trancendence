3879ed125a692fc47dd11e5058f2a4b0573c071b
Araiva <kizzaraiva@gmail.com>
2023-10-06 11:29:21 +0700

    feat: create 2fa entity

--- backend/src/features/me/security/security.service.ts
+++ backend/src/features/me/security/security.service.ts
@@ -1,16 +1,33 @@
-
+import { Repository } from 'typeorm';
+import { User2fa } from '@backend/typeorm/user_2fa.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { authenticator } from 'otplib';
 
 @Injectable()
 export class SecurityService {
-  constructor() {}
+  public static readonly MAX_DEVICE = 1;
+  constructor(
+    @InjectRepository(User2fa) private user2faRepository: Repository<User2fa>,
+  ) {}
 
-  generatSecret() {
+  // https://wanago.io/2021/03/08/api-nestjs-two-factor-authentication/
+  get2faDevices(userId: number) {
+    return this.user2faRepository.find({
+      relations: { user: true },
+      where: { user: { id: userId } },
+    });
+  }
+
+  register2faDevice(userId: number) {
     const secret = 'KVKFKRCPNZQUYMLXOVYDSQKJKZDTSRLD';
     const token = authenticator.generate(secret);
     const genSecret = authenticator.generateSecret();
-    const otpauthUrl = authenticator.keyuri('tsomsa', 'ft_transcendence', secret);
-    return {genSecret, token, otpauthUrl};
+    const otpAuthUrl = authenticator.keyuri(username, process.env.PROJECT_NAME, secret);
+    return { genSecret, token, otpAuthUrl };
+  }
+
+  activate2faDevice(id: number) {
+    return '';
   }
 }
