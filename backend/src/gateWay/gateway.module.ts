478e0d7993b59d486f6a93ad96742f6a4bd17f52
abossel <116184157+abossel@users.noreply.github.com>
2023-12-28 18:10:54 +0700

    more game features (#125)

--- backend/src/gateWay/gateway.module.ts
+++ backend/src/gateWay/gateway.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SessionGateway } from './session/session.geteway';
 import { UserSessionModule } from '@backend/features/user-session/user-session.module';
+import { SharedModule } from '@backend/shared/shared.module';
 
 @Module({
   providers: [SessionGateway],
-  imports: [UserSessionModule],
+  imports: [UserSessionModule, SharedModule],
 })
 export class GatewayModule {}
