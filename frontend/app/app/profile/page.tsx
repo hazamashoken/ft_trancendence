4cce8674f19906cb431717e4569f2fc55e5cf8b1
hazamashoken <46592735+hazamashoken@users.noreply.github.com>
2023-12-29 20:24:00 +0800

    quick fix (#137)

--- frontend/app/app/profile/page.tsx
+++ frontend/app/app/profile/page.tsx
@@ -11,9 +11,20 @@ export default function Home() {
             <div>loading</div>
         )
     }
+    const users = [
+        {id: '1', intraLogin: 'abced' },
+        {id: '2', intraLogin: 'xyz' },
+        {id: '3', intraLogin: 'yuio '},
+    ];
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <UserCard session={session}/>
+            {
+                users.map(user => (
+                    <div key={user.id} className="border-2 border-sky-500">
+                        {user.id} - {user.intraLogin}
+                    </div>
+                ))
+            }
         </main>
     )
 }
